b14793bb842141728b1f741691029a545ecd69c5
kulig-dev <robert.kulig@post.cz>
2024-05-21 23:19:42 +0200

    Adding Http Client Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  baseURl = "";
+  products:[] = [];
+  constructor(private http:HttpClient) {
+
+  }
+  getProduct() {
+    return this.http.get(this.baseURl).subscribe(
+      (values:any) => {
+        this.products = values;
+      }
+    );
+  }
+  ngOnInit(): void {
+    this.getProduct();
+  }
+
 }
