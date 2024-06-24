9f578114b57013c633ef49a38f66262984f9b975
kulig-dev <robert.kulig@post.cz>
2024-06-24 23:57:26 +0200

    Refactoring Pagination with Paging Header Component

---
+++ src/app/shared/Components/paging-header/paging-header.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paging-header',
+  templateUrl: './paging-header.component.html',
+  styleUrl: './paging-header.component.scss'
+})
+export class PagingHeaderComponent implements OnInit {
+
+  @Input() pageNumber: number;
+  @Input() pageSize: number;
+  @Input() totalCount: number;
+  constructor() { }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
