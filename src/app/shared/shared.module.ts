d06f45d669fba9363e84b8d58dc1560c8491aca8
kulig-dev <robert.kulig@post.cz>
2024-06-26 22:33:46 +0200

    Refactoring Pagination: Introduce Pager Component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './Components/paging-header/paging-header.component';
+import { PagerComponent } from './Components/pager/pager.component';
 
 
 
 @NgModule({
   declarations: [
-    PagingHeaderComponent
+    PagingHeaderComponent,
+    PagerComponent
   ],
   imports: [
     CommonModule,
@@ -15,7 +17,8 @@ import { PagingHeaderComponent } from './Components/paging-header/paging-header.
   ],
   exports: [
     PaginationModule,
-    PagingHeaderComponent
+    PagingHeaderComponent,
+    PagerComponent
   ]
 })
 export class SharedModule { }
